37e19f616a2e15e0ee43051378ce3d451cb08806
David <david.sosa@protonmail.com>
2020-04-02 10:33:35 +0200

    Changed previous version of ukf files

--- src/ukf.h
+++ src/ukf.h
@@ -1,111 +1,148 @@
-#ifndef UKF_H
-#define UKF_H
+#pragma once
 
-#include "measurement_package.h"
+#include <iostream>
 #include "Eigen/Dense"
-#include <vector>
-#include <string>
-#include <fstream>
-
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
+#include "measurement_package.h"
 
 class UKF {
 public:
+    /**
+     * Constructor
+     */
+    UKF();
 
-  ///* initially set to false, set to true in first call of ProcessMeasurement
-  bool is_initialized_;
+    /**
+     * Destructor
+     */
+    virtual ~UKF() = default;
 
-  ///* if this is false, laser measurements will be ignored (except for init)
-  bool use_laser_;
+    /**
+     * ProcessMeasurement
+     * @param meas_package The latest measurement data of either radar or laser
+     */
+    void ProcessMeasurement(MeasurementPackage meas_package);
 
-  ///* if this is false, radar measurements will be ignored (except for init)
-  bool use_radar_;
-
-  ///* state vector: [pos1 pos2 vel_abs yaw_angle yaw_rate] in SI units and rad
-  VectorXd x_;
-
-  ///* state covariance matrix
-  MatrixXd P_;
-
-  ///* predicted sigma points matrix
-  MatrixXd Xsig_pred_;
-
-  ///* time when the state is true, in us
-  long long time_us_;
-
-  long long previous_t;
-
-  long long delta_t;
- 
-  ///* Process noise standard deviation longitudinal acceleration in m/s^2
-  double std_a_;
-
-  ///* Process noise standard deviation yaw acceleration in rad/s^2
-  double std_yawdd_;
-
-  ///* Laser measurement noise standard deviation position1 in m
-  double std_laspx_;
-
-  ///* Laser measurement noise standard deviation position2 in m
-  double std_laspy_;
-
-  ///* Radar measurement noise standard deviation radius in m
-  double std_radr_;
-
-  ///* Radar measurement noise standard deviation angle in rad
-  double std_radphi_;
-
-  ///* Radar measurement noise standard deviation radius change in m/s
-  double std_radrd_ ;
-
-  ///* Weights of sigma points
-  VectorXd weights_;
-
-  ///* State dimension
-  int n_x_;
-
-  ///* Augmented state dimension
-  int n_aug_;
-
-  ///* Sigma point spreading parameter
-  double lambda_;
+    /**
+     * Prediction Predicts sigma points, the state, and the state covariance
+     * matrix
+     * @param dt Time between k and k+1 in s
+     */
+    void Predict(double dt);
 
 
-  /**
-   * Constructor
-   */
-  UKF();
+    /**
+     * @brief Generates sigma points and augments with the process noise vector
+     * 
+     * @param Xsig_out 
+     */
+    void GenerateAugmentedSigmaPoints(Eigen::MatrixXd& Xsig_out);
 
-  /**
-   * Destructor
-   */
-  virtual ~UKF();
+    /**
+     * @brief Passes the generated sigma points through the (non-linear) prediction function f
+     * 
+     * @param Xsig_aug 
+     * @param dt 
+     */
 
-  /**
-   * ProcessMeasurement
-   * @param meas_package The latest measurement data of either radar or laser
-   */
-  void ProcessMeasurement(MeasurementPackage meas_package);
+    void SigmaPointPrediction(Eigen::MatrixXd& Xsig_aug, double dt);
+    
+    /**
+     * @brief Predict the mean and the covariants using the weights 
+     * 
+     */
 
-  /**
-   * Prediction Predicts sigma points, the state, and the state covariance
-   * matrix
-   * @param delta_t Time between k and k+1 in s
-   */
-  void Prediction(double delta_t);
+    void PredictMeanAndCovariance();
+    
+    /**
+     * @brief 
+     * 
+     */
+    void PredictRadarMeasurement();
+    
+    /**
+     * @brief 
+     * 
+     */
+    void PredictLaserMeasurement();
+    
+    void UpdateState(const Eigen::VectorXd& z);
 
-  /**
-   * Updates the state and the state covariance matrix using a laser measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateLidar(MeasurementPackage meas_package);
 
-  /**
-   * Updates the state and the state covariance matrix using a radar measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateRadar(MeasurementPackage meas_package);
+    // initially set to false, set to true in first call of ProcessMeasurement
+    bool is_initialized_;
+
+    // if this is false, laser measurements will be ignored (except for init)
+    bool use_laser_;
+
+    // if this is false, radar measurements will be ignored (except for init)
+    bool use_radar_;
+
+    // state vector: [pos1 pos2 vel_abs yaw_angle yaw_rate] in SI units and rad
+    Eigen::VectorXd x_;
+
+    // state covariance matrix
+    Eigen::MatrixXd P_;
+
+    // predicted sigma points matrix
+    Eigen::MatrixXd Xsig_pred_;
+
+    // Process noise standard deviation longitudinal acceleration in m/s^2
+    double std_a_;
+
+    // Process noise standard deviation yaw acceleration in rad/s^2
+    double std_yawdd_;
+
+    // Laser measurement noise standard deviation position1 in m
+    double std_laspx_;
+
+    // Laser measurement noise standard deviation position2 in m
+    double std_laspy_;
+
+    // Radar measurement noise standard deviation radius in m
+    double std_radr_;
+
+    // Radar measurement noise standard deviation angle in rad
+    double std_radphi_;
+
+    // Radar measurement noise standard deviation radius change in m/s
+    double std_radrd_ ;
+
+    // Weights of sigma points
+    Eigen::VectorXd weights_;
+
+    // State dimension
+    int n_x_;
+
+    // Augmented state dimension
+    int n_aug_;
+
+    // Sigma point spreading parameter
+    double lambda_;
+
+    // current NIS for radar
+    double NIS_radar_;
+
+    // current NIS for laser
+    double NIS_laser_;
+
+private:
+    // previous timestamp
+    long previous_timestamp_;
+
+    //set measurement dimension, radar can measure r, phi, and r_dot
+    int n_z_;
+
+    //create matrix for sigma points in measurement space
+    Eigen::MatrixXd Zsig_;
+
+    //mean predicted measurement
+    Eigen::VectorXd z_pred_;
+
+    //measurement covariance matrix S
+    Eigen::MatrixXd S_;
+
+    //measurement noise covariance matrix
+    Eigen::MatrixXd R_laser_, R_radar_;
+
 };
 
-#endif /* UKF_H */
